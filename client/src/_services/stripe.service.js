921cccd4a0b61bc60353076e9cbca35903c89627
Diego Mendez <diegomarmcfly@gmail.com>
2019-03-17 23:22:16 -0600

    login added

--- client/src/_services/stripe.service.js
+++ client/src/_services/stripe.service.js
@@ -7,7 +7,8 @@ export const sendPayment = token => {
         method : 'POST',
         headers : {...authHeader(), "Content-Type": "text/plain"},
         // headers : {"Content-Type": "text/plain"},
-        body : { stripeEmail : "admin@admin.com" , token}
+        // body : { stripeEmail : "admin@admin.com" , token}
+        body : token.id
     }
 
     return fetch(`https://oobie92.com/api/stripe/charge/`, requestOptions).then(handleResponse)
